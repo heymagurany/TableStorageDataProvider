3b5f682d5462984254c242125007338159221124
Matt Magurany/n <honkywater@gmail.com>
2012-09-08 00:32:39 -0400

    Updated the build.targets file to copy the assembly from the correct directory.

--- src/TableStorageTest/Program.cs
+++ src/TableStorageTest/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.Common;
-using System.Linq;
-using System.Text;
-using Microsoft.WindowsAzure;
 
 namespace Magurany.Data.TableStorageClient.Test
 {
