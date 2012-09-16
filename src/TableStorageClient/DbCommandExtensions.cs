7b80873d42f0cccfa099b73de16ee930c47131ff
Matt Magurany/n <honkywater@gmail.com>
2012-09-16 18:44:06 -0400

    Added the DbCommandExtensions classes, which contains helper methods for adding parameters to a command.

---
+++ src/TableStorageClient/DbCommandExtensions.cs
@@ -0,0 +1,68 @@
+﻿using System.Data;
+using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Magurany.Data.TableStorageClient
+{
+	[SuppressMessage("Microsoft.Naming", "CA1709:IdentifiersShouldBeCasedCorrectly", MessageId = "Db", Justification = "Following .NET Framework standards.")]
+	public static class DbCommandExtensions
+	{
+		public static DbParameter AddMappedParameter(this DbCommand command, string parameterName, DbType dbType, string sourceColumn)
+		{
+			ThrowHelper.CheckArgumentNull(command, "command");
+
+			DbParameter parameter = command.CreateParameter();
+			parameter.DbType = dbType;
+			parameter.ParameterName = parameterName;
+			parameter.SourceColumn = sourceColumn;
+
+			command.Parameters.Add(parameter);
+
+			return parameter;
+		}
+
+		public static DbParameter AddMappedParameter(this DbCommand command, string parameterName, DbType dbType, int size, string sourceColumn)
+		{
+			ThrowHelper.CheckArgumentNull(command, "command");
+
+			DbParameter parameter = command.CreateParameter();
+			parameter.DbType = dbType;
+			parameter.ParameterName = parameterName;
+			parameter.Size = size;
+			parameter.SourceColumn = sourceColumn;
+
+			command.Parameters.Add(parameter);
+
+			return parameter;
+		}
+
+		public static DbParameter AddParameter(this DbCommand command, string parameterName, DbType dbType, object value)
+		{
+			ThrowHelper.CheckArgumentNull(command, "command");
+
+			DbParameter parameter = command.CreateParameter();
+			parameter.DbType = dbType;
+			parameter.ParameterName = parameterName;
+			parameter.Value = value;
+
+			command.Parameters.Add(parameter);
+
+			return parameter;
+		}
+
+		public static DbParameter AddParameter(this DbCommand command, string parameterName, DbType dbType, int size, object value)
+		{
+			ThrowHelper.CheckArgumentNull(command, "command");
+
+			DbParameter parameter = command.CreateParameter();
+			parameter.DbType = dbType;
+			parameter.ParameterName = parameterName;
+			parameter.Size = size;
+			parameter.Value = value;
+
+			command.Parameters.Add(parameter);
+
+			return parameter;
+		}
+	}
+}
