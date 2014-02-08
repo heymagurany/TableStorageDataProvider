dafd62cb8907c433b36b6349b4af1c99728842e7
Matt Magurany <mmagurany@live.com>
2014-02-08 16:21:21 -0500

    Update project meta data.

--- src/TableStorageClient/Properties/AssemblyInfo.cs
+++ src/TableStorageClient/Properties/AssemblyInfo.cs
@@ -2,5 +2,5 @@
 using System.Runtime.InteropServices;
 
 [assembly: AssemblyTitle("Windows Azure Table Storage ADO.NET Data Provider")]
-[assembly: AssemblyDescription(".NET Framework data provder intended to allow developers to write ADO.NET code against Windows Azure Table Storage.")]
+[assembly: AssemblyDescription(".NET Framework data provider intended to allow developers to write ADO.NET code against Windows Azure Table Storage.")]
 [assembly: ComVisible(false)]
